5f412ecc0d96ca83a5531e230c1f3306eea11e60
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-09-14 15:53:53 +0800

    done with exercise2.5

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#exercise2.4
-a = int(input("please give a number"))
-b = int(input("please give a number"))
-c = int(input("please give a number"))
-sum = a+b+c
-product = a*b*c
-average = sum/3
-print(sum,product,average)
+#exercise2.5
+talents = input("Enter talents")
+pounds = input("Enter pounds")
+lots = input("Enter lots")
+grams = 20*32*13.3*float(talents)+32*13.3*float(pounds)+13.3*float(lots)
+kilograms = int(10**-3*grams)
+grams_left = grams % 1000
+print(f"The weight in modern units: {kilograms} kilograms and {grams_left:.2f}grams")
+
