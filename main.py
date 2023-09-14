d2fc9569d60536984af31cc621e6e02ade164c64
xixihahagugu <Mengna.Deng@metropolia.fi>
2023-09-14 16:43:43 +0800

    done with exercise2.6

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#exercise2.5
-talents = input("Enter talents")
-pounds = input("Enter pounds")
-lots = input("Enter lots")
-grams = 20*32*13.3*float(talents)+32*13.3*float(pounds)+13.3*float(lots)
-kilograms = int(10**-3*grams)
-grams_left = grams % 1000
-print(f"The weight in modern units: {kilograms} kilograms and {grams_left:.2f}grams")
+#exercise2.6
+import random
 
+print( f"{random.randint(0,999):03d} ")
+
+import random
+
+print(str(random.randint(1,6))+str(random.randint(1,6))+str(random.randint(1,6))+str(random.randint(1,6)))
